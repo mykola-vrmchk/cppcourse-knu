63d23e7954b118c1b0f05c2adfbe96ceebda9fee
Mykola Veremchuk <veremchukmukola@gmail.com>
2025-02-12 11:26:59 +0200

    control exercise

--- exercises/control/control.cpp
+++ exercises/control/control.cpp
@@ -7,13 +7,8 @@ bool isodd(unsigned int i) { return i % 2 == 1; }
 void part1() {
   unsigned int sum_odd = 0;
   unsigned int sum_eve = 0;
-  for (int i = 0; i < 9; ++i) {
-    unsigned int num = numbers[i];
-    if (isodd(num)) {
-      sum_odd += num;
-    } else {
-      sum_eve += num;
-    }
+  for (unsigned int number : numbers) {
+    (isodd(number) ? sum_odd : sum_eve) += number;
   }
   std::cout << "Sums: odd = " << sum_odd << ", even = " << sum_eve << "\n";
 }
@@ -21,28 +16,32 @@ void part1() {
 void part2() {
   // print smallest n for which 1 + 2 + ... + n > 10000
   int sum = 0;
-  for (int i = 1; ; i++) {
+  int i = 0;
+  while (sum <= 10000) {
+    ++i;
     sum += i;
-    if (sum > 10000) {
-      std::cout << i << "\n";
-      break;
-    }
   }
+  std::cout << "Last i: " << i << "\n";
 }
 
 enum class Language { English, French, German, Italian, Other };
 
 void part3(Language l) {
-  if (l == Language::English) {
+  switch (l) {
+  case Language::English:
     std::cout << "Hello\n";
-  } else if (l == Language::French) {
+    break;
+  case Language::French:
     std::cout << "Salut\n";
-  } else if (l == Language::German) {
+    break;
+  case Language::German:
     std::cout << "Hallo\n";
-  } else if (l == Language::Italian) {
+    break;
+  case Language::Italian:
     std::cout << "Ciao\n";
-  } else {
-    std::cout << "I don't speak your language\n";
+    break;
+  default:
+    std::cout << "Don't speak your language\n";
   }
 }
 
